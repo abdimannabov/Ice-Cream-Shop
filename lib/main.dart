e1db72fa9c297c979a887f08f59dfd62c97ddbf3
Sohib <abdumanobovsohib13@gmail.com>
2025-09-10 09:30:34 +0500

    Initial commit for new Flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/pages/toDo_page.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -11,8 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ToDoPage(),
+      debugShowCheckedModeBanner: false, 
+      home: Scaffold()
     );
   }
 }
