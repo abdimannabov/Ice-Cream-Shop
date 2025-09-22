ca93e533570b25cdc13686291b8dc60bdad575fa
Sohib <abdumanobovsohib13@gmail.com>
2025-09-22 10:34:32 +0500

    Created UI part for user sign in/up. But the functionality isn't finished yet

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/blocs/authentication/authentication_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'screens/authentication/views/welcome_screen.dart';
+import 'screens/home/views/home_screen.dart';
 
 class MyAppView extends StatelessWidget {
   const MyAppView({super.key});
@@ -8,7 +12,23 @@ class MyAppView extends StatelessWidget {
     return MaterialApp(
       title: 'Ice-Cream Shop',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Text("Hello World!")),
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          background: Colors.grey.shade100,
+          onBackground: Colors.black,
+          primary: Colors.blue,
+          onPrimary: Colors.white,
+        ),
+      ),
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: (context, state) {
+          if (state.status == AuthenticationStatus.authenticated) {
+            return HomeScreen();
+          } else {
+            return WelcomeScreen();
+          }
+        },
+      ),
     );
   }
 }
