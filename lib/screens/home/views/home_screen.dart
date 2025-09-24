32c778dd9d0e1a387a79cbd3162eececcdf4dc53
Sohib <abdumanobovsohib13@gmail.com>
2025-09-24 14:40:39 +0500

    The home screen UI part fully completed

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -12,31 +12,26 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.background,
-        title: Row(children: [
-          Image.asset(
-            'assets/3.jpeg',
-            scale: 4,
-          ),
-          SizedBox(width: 8,),
-          Text(
-            'Ice-Cream',
-            style: TextStyle(
-              fontWeight: FontWeight.w900,
-              fontSize: 30,
+        title: Row(
+          children: [
+            Image.asset('assets/logo.png', 
+              height: 50,
+              width: 50,
+              scale: 3),
+            SizedBox(width: 8),
+            Text(
+              'Ice-Cream',
+              style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30),
             ),
-          ),
-        ],
+          ],
         ),
         actions: [
+          IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.cart)),
           IconButton(
-            onPressed: (){}, 
-            icon: Icon(CupertinoIcons.cart)
-          ),
-          IconButton(
-            onPressed: (){
+            onPressed: () {
               context.read<SignInBloc>().add(SignOutRequired());
-            }, 
-            icon: Icon(CupertinoIcons.arrow_right_to_line)
+            },
+            icon: Icon(CupertinoIcons.arrow_right_to_line),
           ),
         ],
       ),
@@ -45,71 +40,133 @@ class HomeScreen extends StatelessWidget {
           crossAxisCount: 2,
           crossAxisSpacing: 10,
           mainAxisSpacing: 10,
-          childAspectRatio: 9/16
-        ), 
+          childAspectRatio: 9 / 16,
+        ),
         itemCount: 4,
         itemBuilder: (context, int i) {
-          return Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [BoxShadow(
-                color: Colors.grey.shade300,
-                blurRadius: 5,
-                offset: Offset(3, 3),
-              )]
+          return Material(
+            elevation: 3,
+            color: Colors.pink.shade50,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadiusGeometry.circular(24),
             ),
-            child: Column(
-              children: [
-                Image.asset(
-                  'assets/3.jpeg',
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12),
-                  child: Row(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: Colors.pinkAccent
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 6),
-                          child: Text(
-                            "Strawberry",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 12,
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: 8,),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: Colors.blue.shade100
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 6),
-                          child: Text(
-                            "Pure Milk",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w900,
-                              fontSize: 12,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+            child: InkWell(
+              onTap: () {
+                
+              },
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.asset('assets/8.png',
+                    width: 220,
+                    height: 220,
                   ),
-                )
-              ],
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12),
+                    child: Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.pinkAccent,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 4.0,
+                              vertical: 6,
+                            ),
+                            child: Text(
+                              "Strawberry",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 8),
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.blue.shade100,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 4.0,
+                              vertical: 6,
+                            ),
+                            child: Text(
+                              "Pure Milk",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w900,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 8,),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12),
+                    child: Text(
+                      "Vanilla Ice-Cream",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12),
+                    child: Text(
+                      "Very delicious Ice-Cream with fruits!",
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey.shade700
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              "\$8.00",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.green.shade600,
+                              ),
+                            ),
+                            SizedBox(width: 5,),
+                            Text(
+                              "\$10.00",
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.grey.shade500,
+                                decoration: TextDecoration.lineThrough
+                              ),
+                            )
+                          ],
+                        ),
+                        IconButton(
+                          onPressed: (){}, 
+                          icon: Icon(CupertinoIcons.add_circled_solid)
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           );
-        }
+        },
       ),
     );
   }
